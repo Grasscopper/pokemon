181205acf7ae6f9670a22c6a7581c240d162c545
Justin Stransky <dva@colostate.edu>
2025-06-28 21:55:56 -0400

    Create pagination

---
+++ src/app/Pagination.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React from "react";
+
+const Pagination = (props: { totalPokemon: number; setCurrentPage: (arg0: number) => void; currentPage: number; }) => {
+  const numPages = Math.ceil(props.totalPokemon/25);
+  let pageNumbers = [];
+  for (let number = 1; number < numPages + 1; number++) {
+    pageNumbers.push(number);
+  }
+
+  const navigate = (event: { currentTarget: { innerHTML: any; }; }) => {
+    props.setCurrentPage(event.currentTarget.innerHTML);
+  }
+
+  const prev = () => {
+    if (props.currentPage != 1) {
+      props.setCurrentPage(props.currentPage - 1);
+    }
+  }
+
+  const next = () => {
+    if (props.currentPage != numPages) {
+      props.setCurrentPage(props.currentPage + 1);
+    }
+  }
+
+  const pages = pageNumbers.map((page) => {
+    let current = "";
+    if (page == props.currentPage) current = "is-current";
+    return (
+      <li key={page}>
+        <a onClick={navigate} className={`pagination-link ${current}`} aria-label={`Goto page ${page}`}>{page}</a>
+      </li>
+    )
+  })
+
+  return (
+    <nav className="pagination is-medium" role="navigation" aria-label="pagination">
+      <a className="pagination-previous" onClick={prev}>Previous</a>
+      <a className="pagination-next" onClick={next}>Next page</a>
+      <ul className="pagination-list">{pages}</ul>
+    </nav>
+  )
+}
+
+export default Pagination
